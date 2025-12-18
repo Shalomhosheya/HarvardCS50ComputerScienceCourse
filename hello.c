0b05216ec25cba35b2cb5cb58610a26197e59ce5
KD ShalomHosheya Jayathilaka <129625075+Shalomhosheya@users.noreply.github.com>
2025-12-18 23:11:36 +0530

    name inputsm

--- hello.c
+++ hello.c
@@ -1,5 +1,10 @@
+#include <cs50.h>
 #include <stdio.h>
 
 int main(void) {
-    printf("Hello, World!\n");
-}
+    string answer = get_string("what is your first name ");
+    string answer2 = get_string("what is your last name ");
+    printf("Hello,%s %s\n",answer,answer2);
+}   
+//could run in the bottom codespace
+// https://cs50.dev 
