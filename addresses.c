9f5ed8d4bd923c9672946cb6a107d0471b650e22
KD ShalomHosheya Jayathilaka <129625075+Shalomhosheya@users.noreply.github.com>
2025-12-27 00:51:43 +0530

    addresses as * symbol and & symbol

--- addresses.c
+++ addresses.c
@@ -4,5 +4,6 @@ int main()
 {
     int a = 10;
     int *p = &a;
-    printf("The address of a is %p\n", p);
+    printf("The address of a is %p\n", p); // %p is used to print the address
+    printf("The address of a is %i\n", *p); // *p is used to print the value at the address
 }
