1e4ebe53e35324e4d49e1bb6ffc106341630f210
KD ShalomHosheya Jayathilaka <129625075+Shalomhosheya@users.noreply.github.com>
2025-12-30 02:06:44 +0530

    malloc function to copy memory allocation address

--- copy.c
+++ copy.c
@@ -8,7 +8,7 @@ int main(void)
     char *s = get_string("Copy from: ");
     char *t = malloc(strlen(s+1));
 
-    for (int i = 0; i < strlen(s)+1; i++)
+    for (int i = 0, n=strlen(s)+1; i < n; i++)
     {
         t[i] = s[i];
     }
