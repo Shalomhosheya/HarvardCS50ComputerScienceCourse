039d495352a90e8f8bc481de630a8ba9d70a531e
KD ShalomHosheya Jayathilaka <129625075+Shalomhosheya@users.noreply.github.com>
2026-01-01 23:25:04 +0530

    copy value with memory allocation

--- list.c
+++ list.c
@@ -1,5 +1,5 @@
-#include <stdio.lb>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
     int *list=malloc(3*sizeof(int));
@@ -11,5 +11,26 @@ int main(){
     {
         return 1;
     }
+    int *tmp = malloc(4*sizeof(int));
+    if(tmp == NULL){
+        free(list);
+        return 1;
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        tmp[i] = list[i];
+    }
+
+    tmp[3]= 4 ;
+    free(list);
+    list = tmp;
+    for (int i = 0; i < 4; i++)
+    {
+        printf("%i\n",list[i]);
+    }
+
+
+    free(list);
+    return 0;
     
 }
