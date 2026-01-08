594ef9b38d78b03010f7f93b29178ccceb2ecf3b
KD ShalomHosheya Jayathilaka <129625075+Shalomhosheya@users.noreply.github.com>
2026-01-08 20:22:03 +0530

    mario implementation

--- greet.py
+++ greet.py
@@ -1,3 +1,10 @@
 from sys import argv
+import sys
 for arg in argv[0:3]:
     print(arg)
+    
+if len(argv) != 2:
+    print("missing command-line argument")
+    sys.exit(1);
+print("Hello, " + argv[1] + "!")
+sys.exit(0)
