64eb1523d24f13cdcdeeec921944a567ec471111
KD ShalomHosheya Jayathilaka <129625075+Shalomhosheya@users.noreply.github.com>
2026-01-11 21:54:13 +0530

    Sequel language introduction

--- favourites_py.py
+++ favourites_py.py
@@ -4,4 +4,11 @@ with open("favourites.csv","r") as file:
     reader = csv.DictReader(file)
     for row in reader:
         fav = row["name"]
-        print(fav)
+        name,email = 0,0
+        # print(fav)
+        if fav == "Shalom Hosheya":
+            name +=1
+        elif fav == "shalom@example.com":
+            email +=1
+    print(f"same name {name}")
+    print(f"same email {email}")
