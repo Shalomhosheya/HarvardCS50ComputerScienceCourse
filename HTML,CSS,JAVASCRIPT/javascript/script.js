e0ae141d7671129488071e31ee7932a06b4f77d8
KD ShalomHosheya Jayathilaka <129625075+Shalomhosheya@users.noreply.github.com>
2026-01-15 18:26:51 +0530

    html,css,javascript intro

--- HTML,CSS,JAVASCRIPT/javascript/script.js
+++ HTML,CSS,JAVASCRIPT/javascript/script.js
@@ -1,4 +1,9 @@
-function greet(){
-            let name = document.querySelector("#name").value;
-            alert("Hello "+ name + "!");
-        }
+// function greet(){
+//             let name = document.querySelector("#name").value;
+//             alert("Hello "+ name + "!");
+//         }
+document.querySelector('form').addEventListener('submit', function(){
+   let name = document.querySelector("#name").value;
+   alert("Hello "+ name + "!");
+   event.preventDefault();
+});
