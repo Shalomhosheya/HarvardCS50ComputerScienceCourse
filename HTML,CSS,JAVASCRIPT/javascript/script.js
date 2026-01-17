dc0a064d9d5bd2a59344a21589289c76aaaeb021
KD ShalomHosheya Jayathilaka <129625075+Shalomhosheya@users.noreply.github.com>
2026-01-17 17:28:29 +0530

    flask in  python

--- HTML,CSS,JAVASCRIPT/javascript/script.js
+++ HTML,CSS,JAVASCRIPT/javascript/script.js
@@ -10,3 +10,13 @@ document.querySelector('form').addEventListener('submit', function(){
    event.preventDefault();
 });
     });
+
+//         function blink(){
+//         let body = document.querySelector("body");
+//         if(body.style.visibility == "hidden"){
+//             body.style.visibility = "visible";
+//         }else{
+//             body.style.visibility = "hidden";
+//         }
+//     }
+//  window.setInterval(blink,500);
