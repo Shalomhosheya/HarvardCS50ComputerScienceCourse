e8044e862da94e1b1726fbfee252260235a1e50b
KD ShalomHosheya Jayathilaka <129625075+Shalomhosheya@users.noreply.github.com>
2026-01-17 17:48:58 +0530

    Flask in Python Introduction

--- Flask/app.py
+++ Flask/app.py
@@ -1,3 +1,9 @@
+# pip install flask
+
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 
 app = Flask(__name__) 
+
+@app.route("/")
+def index():
+    return render_template('index.html')
