a76d7c5df195175dd450773b18fd6146fd834dc9
KD ShalomHosheya Jayathilaka <129625075+Shalomhosheya@users.noreply.github.com>
2026-01-18 23:07:07 +0530

    froshim introductions

--- Froshims/app.py
+++ Froshims/app.py
@@ -5,15 +5,24 @@ from flask import Flask, render_template, request, redirect, url_for, flash, ses
 app = Flask(__name__) 
 
 REGISTRANTS= {}
+SPORTS={
+    "football":"Football",
+    "basketball":"Basketball",
+    "volleyball":"Volleyball"
+}
 
 @app.route("/",methods=["GET","POST"])
 def index():
-    return render_template("index.html")
+    return render_template("index.html",sports=SPORTS)
 
 @app.route("/register",methods=["GET","POST"])
 def register():
     name = request.form.get("name")
+    if not name:
+        return render_template("failure.html")
     sport = request.form.get("sport")
+    if sport not in SPORTS:
+        return render_template("failure.html")
     REGISTRANTS[name] = sport
     return render_template("success.html")
 
