24bb8d00c51c2ae5c35d55b1bd8159a03f1ce6a5
KD ShalomHosheya Jayathilaka <129625075+Shalomhosheya@users.noreply.github.com>
2026-01-18 22:39:54 +0530

    froshim introductions

--- Froshims/app.py
+++ Froshims/app.py
@@ -8,6 +8,13 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/register")
+@app.route("/register",methods=["GET","POST"])
 def register():
+    name = request.form.get("name")
+    sport = request.form.get("sport")
+    registrants[name]=sport
     return render_template("success.html")
+
+@app.route("/registrants",methods=["GET","POST"])
+def registrants():
+    return render_template("registrants.html",registrants=registrants)
