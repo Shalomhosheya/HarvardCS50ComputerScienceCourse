f9290859c8ed5dc0dce1eb1eb856f78bac38257b
KD ShalomHosheya Jayathilaka <129625075+Shalomhosheya@users.noreply.github.com>
2026-01-22 18:19:16 +0530

    add SQL connection to Flask

---
+++ Froshims/SQL/queries.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE registrants;
+use registrants;
+ CREATE TABLE IF NOT EXISTS registrants (
+    ->                 id INT AUTO_INCREMENT PRIMARY KEY,
+    ->                 name VARCHAR(255) NOT NULL,
+    ->                 sport VARCHAR(50) NOT NULL
+    ->             );
+SELECT * FROM registrants;
